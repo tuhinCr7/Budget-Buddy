f2048262d49fbecddd542771b447ee47fc7f1b04
Tuhin Alam Bijoy <tuhinalambijoy@gmail.com>
2026-09-06 04:24:12 +0600

    update to workable

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:5000/api', // Point to backend
+  baseURL: 'http://localhost:5001/api', // Point to backend
   headers: {
     'Content-Type': 'application/json',
   },
